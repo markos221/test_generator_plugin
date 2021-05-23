3c47c1ad4962b77d111b396e707c15be50d22a8c
GitHub Action <action@github.com>
2021-05-23 15:02:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/markos221/testgeneratorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.markos221.testgeneratorplugin.services
+
+import com.github.markos221.testgeneratorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
